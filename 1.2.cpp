dec55bd504da2b0f10989179478e2ac2eaf4b212
RileyYe <57030687+RileyYe@users.noreply.github.com>
2019-12-19 23:07:04 +0800

    Add files via upload

---
+++ 1.2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int max(int a, int b, int c, int d) {
+    int max1, max2;
+    a > b ? max1 = a : max1 = b;
+    c > d ? max2 = c : max2 = d;
+    max1 > max2 ? max1 = max1 : max1 = max2;
+    return max1;
+}//对四个数据比较
+int max2(const vector<int>& vctr) //遍历比较
+{
+    int max = vctr[0];
+    for (auto i : vctr)
+    {
+        i > max ? max = i : max;
+    }
+    return max;
+}
+int max3(const vector<int>& vctr)//sort后取出最后一个元素
+{
+    sort(vctr.begin(),vctr.end());
+    return vctr.back();
+}
+int main() {
+    //从键盘输入四个整数，并输出其中大的数
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max(a, b, c, d);
+}
